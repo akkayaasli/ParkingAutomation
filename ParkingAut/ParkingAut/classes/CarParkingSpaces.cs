0257a8641fdad07f088ddaf59d7a9257506e8d04
asli akkaya <akkayaasli19@gmail.com>
2023-06-10 18:53:49 +0300

    Car Parking Spaces
    Araç park yerleri için bir class oluşturuldu.

--- ParkingAut/ParkingAut/classes/CarParkingSpaces.cs
+++ ParkingAut/ParkingAut/classes/CarParkingSpaces.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,12 @@ namespace ParkingAut.classes
 {
     class CarParkingSpaces
     {
-
+        public int ID { get; set; }
+        [Column(TypeName = "varchar")]
+        [StringLength(70)]
+        public string ParkYerleri { get; set; }
+        [Column(TypeName = "varchar")]
+        [StringLength(70)]
+        public string Durumu { get; set; }
     }
 }
