fe75736ef66b60e071d72c762f04eb766e53493e
asli akkaya <akkayaasli19@gmail.com>
2023-06-10 18:48:35 +0300

    serialNum adding DB
    Seri numaraları için bir class oluşturuldu.

---
+++ ParkingAut/ParkingAut/classes/serialNum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingAut.classes
+{
+    class serialNum
+    {
+        public int ID { get; set; }
+
+        public int MarkaID { get; set; }
+        [Column(TypeName = "varchar")]
+        [StringLength(70)]
+        public string seri { get; set; }
+    }
+}
