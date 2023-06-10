f20c0ceb1cb2be41322e1baf96fbf961fd030eec
asli akkaya <akkayaasli19@gmail.com>
2023-06-10 19:00:58 +0300

    customer adding DB
    Müşteriler için bir class oluşturuldu.

---
+++ ParkingAut/ParkingAut/classes/customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingAut.classes
+{
+    class customer
+    {
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar")]
+    //adsoyad için karakter sınırlaması olmasın.
+
+        public string AdiSoyadi { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(20)]
+
+        public string Telefon { get; set; }
+
+        [Column(TypeName = "varchar")]
+        //adres için karakter sınırlaması olmasın.
+
+        public string Adres { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(150)]
+
+        public string Email { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(250)]
+
+        public string Resim { get; set; }
+
+        public DateTime Tarih { get; set; }
+    }
+}
